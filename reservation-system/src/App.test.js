557af33dfe346448f487337f5ec9375e012d59b5
Zachary Winter <winterzachary19@gmail.com>
2022-12-01 13:43:40 -0600

    addint unit tests

--- reservation-system/src/App.test.js
+++ reservation-system/src/App.test.js
@@ -1,8 +1,18 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders welcome message', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Welcome to XXXXX/i);
+  expect(linkElement).toBeInTheDocument();
+});
+test('renders guest registation button', () =>{
+  render(<App/>)
+  const linkElement = screen.getByText(/Continue as Guest/i)
+  expect(linkElement).toBeInTheDocument();
+});
+test('renders signin button', () =>{
+  render(<App/>)
+  const linkElement = screen.getByText(/Sign In/i);
   expect(linkElement).toBeInTheDocument();
 });
